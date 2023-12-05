f716ee9f061152fd4f392f5b8c26d71179d8b824
Ty M <ty.c.mosby@gmail.com>
2023-12-05 08:10:22 -0500

    Test case amazon script

---
+++ hw_locators.py
@@ -0,0 +1,11 @@
+driver.find_element(By.XPATH, "//i[@role='img']")
+driver.find_element(By.ID, 'ap_email')
+driver.find_element(By.ID, 'continue')
+driver.find_element(By.XPATH, "//a[text()='Conditions of Use']")
+driver.find_element(By.XPATH, "//a[text()='Privacy Notice']")
+driver.find_element(By.XPATH, "//span[text()='Need help']")
+driver.find_element(By.ID, 'auth-fpp-link-bottom')
+driver.find_element(By.ID, 'ap-other-signin-issues-link')
+driver.find_element(By.XPATH, "//a[@class='a-button-text']")
+
+
